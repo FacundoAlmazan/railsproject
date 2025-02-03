05ef26ea396f4cb81330d1b952b2c3a6e70c5b71
Facundo Almazán <facu.almazan@gmail.com>
2025-02-03 05:20:10 -0300

    vista dashboard y detalles de vistas

--- app/controllers/storefront_controller.rb
+++ app/controllers/storefront_controller.rb
@@ -2,16 +2,29 @@ class StorefrontController < ApplicationController
 
   def home
     @q = Product.active.ransack(params[:q])
-    if params[:q].present? && params[:q][:name_or_description_cont].present?
+
+    if params[:q].present?
       @products = @q.result(distinct: true)
+
+      # Filtrar por stock si el checkbox NO está marcado (mostrando solo productos con stock)
+      if params[:q][:with_stock] == "1"
+        @products = @products.joins(:product_variants).where("product_variants.stock > ?", 0).distinct
+      end
+
+      # Filtrar por categoría si se selecciona una
+      if params.dig(:q, :category_id_eq).present?
+        @products = @products.where(category_id: params[:q][:category_id_eq])
+      end
     else
       @products = Product.active
     end
+
     @products = @products.paginate(page: params[:page], per_page: 16)
   end
 
   def show
-    @product = Product.find(params[:id])
+    @product = Product.active.find(params[:id])
+    @variants = @product.product_variants.active
   end
   
 end
